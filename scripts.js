92108b917c516793a3800ebe7e9d33daef25bdf6
arturtinoco <arturguz.tinoco@gmail.com>
2024-10-29 11:02:30 -0300

    Add click event listener to submit button
    - Trigger number generation and result display

--- scripts.js
+++ scripts.js
@@ -21,3 +21,37 @@ let resultRoundIndex = 1
 let drawnList = []
 let delayMS = 2500 // 2500ms -> 2.5s between each draw
 let repeatNumber = document.querySelector("input[type='checkbox']:checked")
+
+// Trigger result interface when clicked on submit button
+drawButton.addEventListener("click", () => {
+    // Switch input area to div#results on first draw
+    if (firstDraw) {
+        form.classList.add("hidden")
+        results.classList.remove("hidden")
+        drawButton.style.marginTop = "1.7rem"
+
+        // Fill drawnList[] with random number(s)
+        generateNumbers()
+
+        // Switch button icon for replay
+        updateButton()
+
+        firstDraw = false
+    } else {
+        // Empty result array each round
+        if (resultRoundIndex > 1) {
+            for (let i = drawnList.length; i >= 0; i--) {
+                drawnList.pop(drawnList)
+            }
+        }
+        generateNumbers()
+    }
+    // Create animated elements to display results
+    drawListedNumbers()
+    
+    // Clean result container for a new round
+    updateResultContainer(resultRoundIndex)
+    
+    // Save numbers drawn quantity so far
+    resultRoundIndex += 1
+})
